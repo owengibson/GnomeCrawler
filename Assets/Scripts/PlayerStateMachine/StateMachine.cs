36028f10d4766db8ffa0ca1917c53362e17997e8
Deathofmatrix <twinsy4@gmail.com>
2024-02-19 17:21:56 +1100

    Added Initial State Machine Framework

--- Assets/Scripts/PlayerStateMachine/StateMachine.cs
+++ Assets/Scripts/PlayerStateMachine/StateMachine.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class StateMachine : MonoBehaviour
+namespace GnomeCrawler
 {
-    
+    public class StateMachine
+    {
+        public State currentState;
+
+        public void Initialise(State startingState)
+        {
+            currentState = startingState;
+            startingState.Enter();
+        }
+
+        public void ChangeState(State newState)
+        {
+            currentState.Exit();
+
+            currentState = newState;
+            newState.Enter();
+        }
+    }
 }
