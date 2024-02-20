70db7128d0065e09dbe88d25a6cb7485689965ba
owengibson <114120616+owengibson@users.noreply.github.com>
2024-02-20 13:44:41 +1100

    Created CardSO and StatsSO
    Also imported OdinInspector
    
    Codecks IDs: $192 , $191

---
+++ Assets/Scripts/Deckbuilding/CardSO.cs
@@ -0,0 +1,30 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GnomeCrawler.Deckbuilding
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class CardSO : SerializedScriptableObject
+    {
+        public string Name;
+        public string Description;
+        public CardCategory Category;
+        [SerializeField] private Stat _statToUpgrade;
+        [SerializeField] private float _value;
+
+        public bool IsPercentUpgrade;
+        public bool IsActivatableCard;
+        [ShowIf("IsActivatableCard")]
+        public float ActiveDuration;
+
+        [HideInInspector]
+        public KeyValuePair<Stat, float> UpgradedStat;
+
+        private void OnValidate()
+        {
+            UpgradedStat = new KeyValuePair<Stat, float> (_statToUpgrade, _value);
+        }
+    }
+}
