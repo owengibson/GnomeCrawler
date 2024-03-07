28b48f7f4b76879ae594e59cc1806dbae84eb691
Deathofmatrix <twinsy4@gmail.com>
2024-03-07 19:18:00 +1100

    Put charcter in new level testing scene

--- Assets/Scripts/ProgressBar/FaceCameraScript.cs
+++ Assets/Scripts/ProgressBar/FaceCameraScript.cs
@@ -4,6 +4,10 @@ public class FaceCameraScript : MonoBehaviour
 {
     public Camera FaceCamera;
 
+    private void Start()
+    {
+        FaceCamera = Camera.main;
+    }
     private void Update()
     {
         transform.LookAt(FaceCamera.transform, Vector3.up);
