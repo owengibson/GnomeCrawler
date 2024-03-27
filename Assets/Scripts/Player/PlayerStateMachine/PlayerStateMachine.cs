43d8a0429728140c76bbf9e6194679b741c3fca7
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2024-03-27 22:07:30 +1100

    solved build issue

--- Assets/Scripts/Player/PlayerStateMachine/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine/PlayerStateMachine.cs
@@ -4,7 +4,6 @@ using GnomeCrawler.Systems;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-//using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
