e463c34d1bb6436aaec692544decf3c20dd85660
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2024-04-10 17:58:44 +1000

    changes to EnemyScripts
    I have started on the EnemyRangedCombat script, but it's not finished.
    Also, I've fixed the hurt flash, I remembered what we talked about - It will now work with any Type of 'Renderer'
    Also, changed the CheckForRayCastHit to not check for a list

---
+++ Assets/Scripts/Enemy/EnemyDamage/EnemyProjectile.cs
@@ -0,0 +1,28 @@
+using GnomeCrawler.Enemies;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GnomeCrawler
+{
+    public class EnemyProjectile : EnemyCombat
+
+    {
+    //    void Start()
+    //    {
+    //        base.InitialiseVariables();
+    //        _canDealDamage = true;
+    //        StartCoroutine(DestroyProjectile());
+    //    }
+
+    //    protected override void CheckForRaycastHit() { }
+
+    //    private IEnumerator DestroyProjectile()
+    //    {
+    //        yield return new WaitForSeconds(4);
+    //        _canDealDamage = false;
+    //        Destroy(this);
+    //    }
+
+    }
+}
--- Assets/Scripts/Enemy/EnemyDamage/EnemyRangedCombat.cs
+++ Assets/Scripts/Enemy/EnemyDamage/EnemyRangedCombat.cs
@@ -1,10 +1,32 @@
-using GnomeCrawler.Deckbuilding;
+using System.Collections;
 using UnityEngine;
 
 namespace GnomeCrawler.Enemies
 {
-    public class EnemyCombat : CombatBrain
+    //public class EnemyProjectile : EnemyCombat
+    //{
+    //    void Start()
+    //    {
+    //        base.InitialiseVariables();
+    //        _canDealDamage = true;
+    //        StartCoroutine(DestroyProjectile());
+    //    }
+
+    //    protected override void CheckForRaycastHit() { }
+
+    //    private IEnumerator DestroyProjectile()
+    //    {
+    //        yield return new WaitForSeconds(4);
+    //        _canDealDamage = false;
+    //        Destroy(this);
+    //    }
+
+    //}
+
+    public class EnemyRangedCombat : CombatBrain
     {
+        //public EnemyProjectile Projectile;
+
         [SerializeField] protected ProgressBar _healthBar;
         [SerializeField] protected Animator _enemyAnim;
         [SerializeField] protected Renderer _meshRenderer;
@@ -16,37 +38,11 @@ namespace GnomeCrawler.Enemies
             _orginalColor = _meshRenderer.material.color;
         }
 
-        protected override void CheckForRaycastHit()
-        {
-
-            Collider[] hitColliders = Physics.OverlapSphere(_originTransform.position, _weaponLength, _layerMask);
-
-            foreach (var hitCollider in hitColliders)
-            {
-
-                if (hitCollider.transform.TryGetComponent(out IDamageable damageable) && !_hasDealtDamage)
-                {
-                    damageable.TakeDamage(_stats.GetStat(Stat.Damage));
-                    _hasDealtDamage = true;
-                }
-            }
-        }
-
         public override void TakeDamage(float amount)
         {
             base.TakeDamage(amount);
             _healthBar.SetProgress(CurrentHealth / _maxHealth);
-            DamageFeedback();
-        }
-
-        public override void StartDealDamage()
-        {
-            base.StartDealDamage();
-        }
-
-        public override void EndDealDamage()
-        {
-            _canDealDamage = false;
+            /*hurtstate*/ DamageFeedback();
         }
 
         public override void Die()
@@ -63,28 +59,30 @@ namespace GnomeCrawler.Enemies
                 faceCamera.FaceCamera = camera;
             }
         }
+        public override void StartDealDamage()
+        {
+            //EnemyProjectile clone = Instantiate(Projectile);
 
-        private void DamageFeedback()
+            /* 
+             * 
+             * instantiate the thing
+             * with the above script
+             * and it's own stats
+             *  
+             */
+        }
+
+        private void DamageFeedback() // move to hurtstate
         {
             _meshRenderer.material.color = Color.black;
             _enemyAnim.SetBool("isDamaged", true);
             Invoke("ResetColour", .15f);
         }
 
-        private void EndHurtAnimation()
+        private void EndHurtAnimation() // move to hurtstate
         {
             _enemyAnim.SetBool("isDamaged", false);
         }
 
-        protected override void OnDrawGizmos()
-        {
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawSphere(_originTransform.position, _weaponLength);
-        }
-
-        private void ResetColour()
-        {
-            _meshRenderer.material.color = _orginalColor;
-        }
     }
 }
