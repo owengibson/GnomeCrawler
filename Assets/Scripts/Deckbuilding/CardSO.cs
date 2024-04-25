a80f71a2b6fe4760090999deb4260ed4b5a23a95
owengibson <114120616+owengibson@users.noreply.github.com>
2024-04-25 10:11:31 +1000

    Improved custom inspector for CardSO

--- Assets/Scripts/Deckbuilding/CardSO.cs
+++ Assets/Scripts/Deckbuilding/CardSO.cs
@@ -10,20 +10,22 @@ namespace GnomeCrawler.Deckbuilding
     {
         public CardType Type;
         public string Name;
+        [Multiline] public string Description;
 
-        [Multiline]
-        public string Description;
-        public CardCategory Category;
-        [SerializeField] private Stat _statToUpgrade;
-        [SerializeField] private float _value;
-
-        public string AbilityClassName;
-        public List<StringFloatPair> AbilityValues = new List<StringFloatPair>();
-
-        public bool IsPercentUpgrade;
-        public bool IsActivatableCard;
+        [ShowIfGroup("S", Condition = "@Type == CardType.Stat || Type == CardType.StatAndAbility")]
+        [BoxGroup("S/Stat Card Parameters")] public CardCategory Category;
+        [BoxGroup("S/Stat Card Parameters")][SerializeField] private Stat _statToUpgrade;
+        [BoxGroup("S/Stat Card Parameters")][SerializeField] private float _value;
+        [BoxGroup("S/Stat Card Parameters")] public bool IsPercentUpgrade;
+        [BoxGroup("S/Stat Card Parameters")] public bool IsActivatableCard;
         [ShowIf("IsActivatableCard")]
-        public float ActiveDuration;
+        [BoxGroup("S/Stat Card Parameters")] public float ActiveDuration;
+
+        [ShowIfGroup("A", Condition = "@Type == CardType.Ability || Type == CardType.StatAndAbility")]
+        [InfoBox("This is case-sensitive and must exactly match the ability's class name.", InfoMessageType.Warning)]
+        [BoxGroup("A/Ability Card Parameters")] public string AbilityClassName;
+        [BoxGroup("A/Ability Card Parameters")] public List<StringFloatPair> AbilityValues = new List<StringFloatPair>(); // could possibly change this to List<KeyValuePair<string, float>>. 
+
 
         [HideInInspector]
         public KeyValuePair<Stat, float> UpgradedStat;
