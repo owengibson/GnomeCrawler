008aac004c59daf61a0963caf8f211c5fc9f5bb0
owengibson <114120616+owengibson@users.noreply.github.com>
2024-04-30 17:25:36 +1000

    Added ambience and started on volume control

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -8,20 +8,57 @@ namespace GnomeCrawler.Audio
 {
     public class AudioManager : Singleton<AudioManager>
     {
+        [Header("Volume")]
+        [Range(0f, 1f)] public float MasterVolume = 1f;
+        [Range(0f, 1f)] public float MusicVolume = 1f;
+        [Range(0f, 1f)] public float AmbienceVolume = 1f;
+        [Range(0f, 1f)] public float SfxVolume = 1f;
+
+        private Bus _masterBus;
+        private Bus _musicBus;
+        private Bus _ambienceBus;
+        private Bus _sfxBus;
+
         private List<EventInstance> _eventInstances = new List<EventInstance>();
         private List<StudioEventEmitter> _eventEmitters = new List<StudioEventEmitter>();
 
         private EventInstance _musicEventInstance;
+        private EventInstance _ambienceEventInstance;
+
+        private void Awake()
+        {
+            _masterBus = RuntimeManager.GetBus("bus:/");
+            _musicBus = RuntimeManager.GetBus("bus:/Music");
+            _ambienceBus = RuntimeManager.GetBus("bus:/Ambience");
+            _sfxBus = RuntimeManager.GetBus("bus:/SFX");
+
+            UpdateBuses();
+        }
 
         private void Start()
         {
             InitialiseMusic(FMODEvents.Instance.GetEventReference("Music"));
+            InitialiseAmbience(FMODEvents.Instance.GetEventReference("Ambience"));
+        }
+
+        public void UpdateBuses()
+        {
+            _masterBus.setVolume(MasterVolume);
+            _musicBus.setVolume(MusicVolume);
+            _ambienceBus.setVolume(AmbienceVolume);
+            _sfxBus.setVolume(SfxVolume);
         }
 
         private void InitialiseMusic(EventReference musicEventReference)
         {
             _musicEventInstance = CreateEventInstance(musicEventReference);
             _musicEventInstance.start();
+        }
+
+        private void InitialiseAmbience(EventReference ambienceEventReference)
+        {
+            _ambienceEventInstance = CreateEventInstance(ambienceEventReference);
+            _ambienceEventInstance.start();
         }
 
         public void SetMusicParameter(PlayerStatus status)
