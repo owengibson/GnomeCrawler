bf57cb67c15683dca05e7b0c837517f693e10659
owengibson <114120616+owengibson@users.noreply.github.com>
2024-05-22 19:27:42 +1000

    Started on pot plant charge

--- Assets/Scripts/Enemy/EnemyStateMachine/EnemyChargeState.cs
+++ Assets/Scripts/Enemy/EnemyStateMachine/EnemyChargeState.cs
@@ -1,4 +1,5 @@
-using Unity.VisualScripting.FullSerializer;
+using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 namespace GnomeCrawler.Enemies
@@ -14,14 +15,21 @@ namespace GnomeCrawler.Enemies
             ctx.IsAttackFinished = false;
             ctx.EnemyAnimator.SetBool("isMoving", true);
             ctx.EnemyAnimator.SetBool("inCombat", false);
+
+            StartCharge();
         }
 
         public override void UpdateState()
         {
             CheckSwitchState();
+
+            ctx.EnemyNavMeshAgent.destination = ctx.PlayerCharacter.transform.position;
         }
 
-        public override void FixedUpdateState() { }
+        public override void FixedUpdateState()
+        {
+
+        }
 
         public override void CheckSwitchState()
         {
@@ -66,6 +74,23 @@ namespace GnomeCrawler.Enemies
             {
                 return false;
             }
+        }
+
+        private void StartCharge()
+        {
+            DOTween.To(() => ctx.EnemyNavMeshAgent.speed, x => ctx.EnemyNavMeshAgent.speed = x, 10f, 3f).SetEase(Ease.InExpo);
+        }
+
+        private IEnumerator Charge()
+        {
+            ctx.EnemyNavMeshAgent.speed = 1f;
+
+            float counter = 0f;
+            while (counter < 3f)
+            {
+                ctx.EnemyNavMeshAgent.speed *= 1.25f;
+            }
+            yield return null;
         }
     }
 }
