0f937a3a7f9c472a5bc448223f74e0bc8c02df63
Deathofmatrix <twinsy4@gmail.com>
2024-06-09 21:37:27 +1000

    Finished random enemy spawning function

--- Assets/Scripts/Rooms/EnemySpawning.cs
+++ Assets/Scripts/Rooms/EnemySpawning.cs
@@ -10,8 +10,9 @@ namespace GnomeCrawler.Rooms
     {
         [SerializeField] private ProBuilderMesh pbMesh; 
         [SerializeField] private float numberOfEnemiesToSpawn;
+        [SerializeField] private GameObject enemyToSpawn;
 
-        /*void Start()
+        void Start()
         {
             if (pbMesh == null)
             {
@@ -26,25 +27,57 @@ namespace GnomeCrawler.Rooms
             {
                 Vector3 spawnPosition = GetRandomSpawnPosition(pbMesh, face);
 
-                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                sphere.transform.position = spawnPosition;
-                sphere.transform.localScale = Vector3.one * 0.1f;
+                /*GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = pbMesh.transform.position + spawnPosition;
+                sphere.transform.localScale = Vector3.one * 3f;*/
+
+                GameObject enemy = Instantiate(enemyToSpawn, pbMesh.transform.position + spawnPosition, Quaternion.identity);
             }
 
             pbMesh.Refresh();
-        }*/
+        }
 
-        /*Vector3 GetRandomSpawnPosition(ProBuilderMesh mesh, Face face)
+        Vector3 GetRandomSpawnPosition(ProBuilderMesh mesh, Face face)
         {
-            Vector3[] verti = ;
+            Vector3[] vertices = mesh.positions.ToArray();
+            int[] indices = face.indexes.ToArray();
 
-            Vector3[] vertices = pbMesh.positions.ToArray();
-            Vector3 v0 = vertices[randomFace.indexesInternal[0]];
-            Vector3 v1 = vertices[randomFace.indexesInternal[1]];
-            Vector3 v2 = vertices[randomFace.indexesInternal[2]];
+            int triangleCount = indices.Length / 3;
 
-            return Vector3.zero;
-        }*/
+            float[] triangleAreas = new float[triangleCount];
+            float totalArea = 0f;
+
+            for (int i = 0; i < triangleCount; i++)
+            {
+                Vector3 p0 = vertices[indices[i * 3]];
+                Vector3 p1 = vertices[indices[i * 3 + 1]];
+                Vector3 p2 = vertices[indices[i * 3 + 2]];
+
+                float area = Vector3.Cross(p1 - p0, p2 - p0).magnitude / 2f;
+                triangleAreas[i] = area;
+                totalArea += area;
+            }
+
+            float randomValue = Random.Range(0f, totalArea);
+            float cumulativeArea = 0f;
+            int selectedTriangleIndex = 0;
+
+            for (int i = 0; i < triangleCount; i++)
+            {
+                cumulativeArea += triangleAreas[i];
+                if (randomValue <= cumulativeArea)
+                {
+                    selectedTriangleIndex = i;
+                    break;
+                }
+            }
+
+            Vector3 pos0 = vertices[indices[selectedTriangleIndex * 3]];
+            Vector3 pos1 = vertices[indices[selectedTriangleIndex * 3 + 1]];
+            Vector3 pos2 = vertices[indices[selectedTriangleIndex * 3 + 2]];
+
+            return GetRandomPointOnTriangle(pos0, pos1, pos2);
+        }
 
 
         Vector3 GetRandomPointOnTriangle(Vector3 v0, Vector3 v1, Vector3 v2)
