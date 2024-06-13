bf4eb452ec5a5a6b72c00a7de732a80ed79662e8
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2024-06-14 00:13:46 +1000

    completing merge

--- Assets/Scripts/Enemy/EnemyDamage/EnemyRangedCombat.cs
+++ Assets/Scripts/Enemy/EnemyDamage/EnemyRangedCombat.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GnomeCrawler.Enemies
 {
@@ -8,14 +10,45 @@ namespace GnomeCrawler.Enemies
         [SerializeField] private GameObject _enemyProjectilePrefab;
         [SerializeField] private float _projectileSpeed = 5f;
         [SerializeField] private Transform _handTransform;
+        private bool _chargingAttack = false;
+        private GameObject _playerCharacter;
 
-        private void Start()
+        // Teleport Variables
+        private float _playersCurrentDistance;
+        private bool _canTele = true; 
+        [SerializeField] private float _needToTeleportRadius;
+        [SerializeField] private float _teleTimer = 1f;
+        private Vector3 _teleRadius;
+        [SerializeField] private float _teleRange = 5f;
+
+
+        private void Start() 
         {
             base.InitialiseVariables();
 
             foreach (Material mat in _meshRenderer.materials)
             {
                 _originalColours.Add(mat.GetColor("_MainColor"));
+            }
+
+            _playerCharacter = GameObject.FindGameObjectWithTag("Player");
+
+        }
+
+        private void Update()
+        {
+            if (_chargingAttack == true)
+            {
+                FacePlayer();
+            }
+
+            // suppress create bullet while teleporting
+
+            _playersCurrentDistance = Vector3.Distance(transform.position, _playerCharacter.transform.position);
+
+            if(_playersCurrentDistance < _needToTeleportRadius)
+            {
+                Invoke("TeleportAway", 1);
             }
         }
 
@@ -38,21 +71,56 @@ namespace GnomeCrawler.Enemies
             CreateBullet();
         }
 
-        private void DamageFeedback() // move to hurtstate
+        private void DamageFeedback() 
         {
             _meshRenderer.material.color = Color.black;
             _enemyAnim.SetBool("isDamaged", true);
             Invoke("ResetColour", .15f);
         }
 
-        private void EndHurtAnimation() // move to hurtstate
+        private void EndHurtAnimation() 
         {
             _enemyAnim.SetBool("isDamaged", false);
         }
 
+
+        private void TeleportAway()
+        {
+            Vector3 point;
+
+            if (IsPositionOnNavMesh(transform.position, _teleRange, out point) && _canTele)
+            {
+                _canTele = false;
+                transform.position = point;
+                StartCoroutine(TeleportCoolDown());
+            }
+        }
+
+        private IEnumerator TeleportCoolDown()
+        {
+            yield return new WaitForSeconds(_teleTimer);
+            _canTele = true; 
+        }
+
+        private bool IsPositionOnNavMesh(Vector3 position, float range, out Vector3 result)
+        {
+            _teleRadius = position + UnityEngine.Random.insideUnitSphere * range;
+
+            NavMeshHit hit;
+            
+            if (NavMesh.SamplePosition(_teleRadius, out hit, 0.1f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+
+            result = Vector3.zero;
+            return false;
+        }
+
         private void CreateBullet()
         {
-            Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform; // COULD USE REFACTORING  
             GameObject projectile = Instantiate(_enemyProjectilePrefab, _handTransform.position, Quaternion.identity);
 
             Vector3 direction = (playerTransform.position - _handTransform.position).normalized;
@@ -60,7 +128,20 @@ namespace GnomeCrawler.Enemies
 
             Rigidbody rb = projectile.GetComponent<Rigidbody>();
             rb.velocity = direction * _projectileSpeed;
+
         }
+
+        private void AttackCharged()
+        {
+            //Debug.Log("charged");
+            _chargingAttack = false;
+        }
+
+        private void ChargingAttack()
+        {
+            _chargingAttack = true;
+        }
+
         private void ResetColour()
         {
             foreach (Material mat in _meshRenderer.materials)
@@ -70,5 +151,13 @@ namespace GnomeCrawler.Enemies
             }
             _originalColorIndex = 0;
         }
+
+        private void FacePlayer()
+        {
+            Transform playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            transform.LookAt(playerTransform);
+        }
+
+
     }
 }
