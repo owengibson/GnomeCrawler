dd2f2616e4fa27e6ab8ad325d3772803471fcd51
Deathofmatrix <twinsy4@gmail.com>
2024-06-20 19:02:18 +1000

    Changed water reset

--- Assets/WaterReset.cs
+++ Assets/WaterReset.cs
@@ -1,18 +1,25 @@
+using GnomeCrawler.Rooms;
 using GnomeCrawler.Systems;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GnomeCrawler
 {
     public class WaterReset : MonoBehaviour
     {
         private Analytics analyticsScript;
+        private EnemySpawning _enemySpawning;
         [SerializeField] private Transform _resetTransform;
+
 
         private void Start()
         {
             analyticsScript = GetComponent<Analytics>();
+            _enemySpawning = transform.parent.GetComponentInChildren<EnemySpawning>();
         }
 
         private void OnEnable()
@@ -27,16 +34,34 @@ namespace GnomeCrawler
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.TryGetComponent(out IDamageable component))
+            StartCoroutine(RespawnPlayer(other));
+            /*string triggerName = gameObject.name;
+            analyticsScript.TrackTriggerEntry(triggerName);*/
+        }
+
+        private IEnumerator RespawnPlayer(Collider other)
+        {
+            if (!other.TryGetComponent(out IDamageable component)) yield break;
+
+            Vector3 freezePos = other.transform.position;
+            freezePos.y = transform.position.y + 3;
+            other.transform.position = freezePos;
+
+            yield return new WaitForSeconds(0.5f);
+
+            Vector3 position = other.transform.position;
+            position.y = transform.position.y + 3;
+
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(position, out hit, 5, NavMesh.AllAreas))
             {
                 component.TakeDamage(1);
                 other.enabled = false;
-                other.transform.position = _resetTransform.position;
+                other.transform.position = hit.position;
                 other.enabled = true;
             }
 
-            /*string triggerName = gameObject.name;
-            analyticsScript.TrackTriggerEntry(triggerName);*/
+            yield return null;
         }
 
         private void SolidifyWater()
