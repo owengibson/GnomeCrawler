d3d759dbcb5504f6d9ad78003b0963f94490a311
owengibson <114120616+owengibson@users.noreply.github.com>
2024-06-24 17:09:17 +1000

    Fixed compatibility between big weapon and spin
    Codecks ID: $1g6

--- Assets/Scripts/Player/PlayerDamage/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerDamage/PlayerCombat.cs
@@ -61,7 +61,7 @@ namespace GnomeCrawler.Player
             }
             else
             {
-                if (Physics.Raycast(_spinWeaponOrigin.position, _spinWeaponOrigin.up, out hit, _weaponSize, _layerMask))
+                if (Physics.Raycast(_spinWeaponOrigin.position, -_spinWeaponOrigin.up, out hit, _weaponSize, _layerMask))
                 {
                     if (hit.transform.TryGetComponent(out IDamageable damageable) && !_damagedGameObjects.Contains(hit.transform.gameObject))
                     {
@@ -70,7 +70,7 @@ namespace GnomeCrawler.Player
                     }
                 }
             }
-            
+
         }
 
         private void DamageHitTarget(RaycastHit hit, IDamageable damageable)
