437e5183730ca387efadbc86346b0fe1fcf77faf
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2024-08-20 21:18:59 +1000

    end scene now linked up to restart the game

---
+++ Assets/Scripts/Systems/EndSceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GnomeCrawler
+{
+    public class EndSceneManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _signButtonsFirst;
+        [SerializeField] private EventSystem _eventSystem;
+
+        void Start()
+        {
+            EventSystem.current.SetSelectedGameObject(_signButtonsFirst);
+        }
+
+    }
+}
